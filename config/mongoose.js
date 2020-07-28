553af70c4628bdcc484b11a1907eab5d8734941a
shvm61 <60419370+shvm61@users.noreply.github.com>
2020-07-28 21:46:49 +0530

    dotenv

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
