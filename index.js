14d3092d3c607c72107568c229290ae93cab2ea9
shvm61 <60419370+shvm61@users.noreply.github.com>
2020-07-28 22:00:13 +0530

    changed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 const db = require("./config/mongoose");
 
 app.use(express.static("./assets"));
