79d7780e7fdf7b2892ee96fde0fc3e883ce0ef93
shvm61 <60419370+shvm61@users.noreply.github.com>
2020-07-28 21:57:50 +0530

    port

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = 8000;
-const expressLayouts = require("express-ejs-layouts");
+const port = process.env.port || 8000;
 const db = require("./config/mongoose");
 
 app.use(express.static("./assets"));
